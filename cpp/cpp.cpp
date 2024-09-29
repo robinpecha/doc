2804d3493c3b46e59bc5e7d6d58eb2105b2c501c
rrob <robinpecha@gmail.com>
2024-09-29 21:36:23 +0200

    Create cpp.cpp

---
+++ cpp/cpp.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+// COMMENTS // KOMENTARE
+// oneline comment
+//  cout << "Hello World!" << "\n\n";
+
+// multiline comment
+/*
+  int a;
+  cin >> a;
+  cout << a;
+*/
+
+// VARIABLE TYPES - https://www.w3schools.com/cpp/cpp_variables.asp
+/*
+int myNum = 5;               // Integer (whole number without decimals)
+double myFloatNum = 5.99;    // Floating point number (with decimals)
+char myLetter = 'D';         // Character
+string myText = "Hello";     // String (text)
+bool myBoolean = true;       // Boolean (true or false) 
+*/
+
+//
+int myNum;
+myNum = 15;
+cout << myNum; 
+
+//
+int myNum2 = 15;  // myNum is 15
+cout << myNum2;
+
+// ZOBRAZOVANI
+int myAge = 35;
+cout << "I am " << myAge << " years old."; 
+
+// NOVY RADEK, TAB
+// \t 	Creates a horizontal tab 	
+// \\ 	Inserts a backslash character (\) 	
+// \"	Inserts a double quote character
+  cout << "Hello World!" << "\n\n";
+  cout << "Hello World 22!" << "\t\t " << "joo\"oo";
+
+
+
+return 0; 
+
+} 
+
